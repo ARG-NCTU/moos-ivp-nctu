4f252329af48a4c2f3838d0ced7270ecce80a924
hchengwang <hchengwang@gmail.com>
2018-01-22 22:45:00 +0800

    add environment.bash to moos-ivp/build

---
+++ environment.bash
@@ -0,0 +1,5 @@
+
+# moos and LCM
+export PATH=$HOME/moos-ivp/build/bin/:$PATH
+export LD_LIBRARY_PATH=$HOME/moos-ivp/build/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=$HOME/moos-ivp/build/lib/pkgconfig:$PKG_CONFIG_PATH
