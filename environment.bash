ba79f5d2af55c99dc049bc78dd3c7573a67bdee4
championway <cpwearth.eed03@g2.nctu.edu.tw>
2018-08-13 20:23:39 +0800

    update new ivp

--- environment.bash
+++
@@ -1,5 +0,0 @@
-
-# moos and LCM
-export PATH=$HOME/moos-ivp/build/bin/:$HOME/moos-ivp/robotx-mit2014/bin/:$PATH
-export LD_LIBRARY_PATH=$HOME/moos-ivp/build/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=$HOME/moos-ivp/build/lib/pkgconfig:$PKG_CONFIG_PATH
